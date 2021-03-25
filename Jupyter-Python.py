b1686718f699ebbd17c0a52ae9fbf760dcbc28df
1994ZZ <81120750+1994ZZ@users.noreply.github.com>
2021-03-25 16:42:12 -0400

    Add files via upload

---
+++ Jupyter-Python.py
@@ -0,0 +1,25 @@
+'''
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+this is a python file
+'''
+
+#let me coding something
+#for test github & Jupyter Notebook
+
+a = 1
+b = 2
+c = 3
+d = int(input("Please enter a number: "))
+
+
+total = a + b +c + d
+
+print("total number :", total)
